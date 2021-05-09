4e5d6350062c8dd1518ab19c0a4bd96455c81338
jizik <jizik13@gmail.com>
2021-05-09 11:56:44 +0300

    Changes

--- backend/index.js
+++ backend/index.js
@@ -319,10 +319,10 @@ app.get('/api/role/:id', async (req, res) => {
   }
 });
 
-app.use(express.static('../frontend/build'));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile('../frontend/build/index.html');
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 app.listen(PORT, () => {
